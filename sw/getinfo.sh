27668c4dc3ee20e1de47c41cff2de5b0ceae67b6
ZipCPU <dgisselq@ieee.org>
2017-08-07 21:15:06 -0400

    Added copyright statements and documentation to the sw scripts

--- sw/getinfo.sh
+++ sw/getinfo.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
-
+################################################################################
+##
+## Filename:	getinfo.sh
+##
+## Project:	Debouncer project, a learning project to learn the impact
+##		of bouncing on logic within your device.
+##
+## Purpose:	To capture data from the debugging bus about a buttons bouncing,
+##		to output that data to the screen, as well as to append a file
+##	with the information collected.
+##
+## Creator:	Dan Gisselquist, Ph.D.
+##		Gisselquist Technology, LLC
+##
+################################################################################
+##
+## Copyright (C) 2017, Gisselquist Technology, LLC
+##
+## This program is free software (firmware): you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or (at
+## your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+## FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+## for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
+## target there if the PDF file isn't present.)  If not, see
+## <http://www.gnu.org/licenses/> for a copy.
+##
+## License:	GPL, v3, as defined and found on www.gnu.org,
+##		http://www.gnu.org/licenses/gpl.html
+##
+##
+################################################################################
+##
+##
 wbregs bounces  | tee -a btndata.txt
 wbregs duration | tee -a btndata.txt
 wbregs scope 0x88000020
--- sw/reset.sh
+++ sw/reset.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
-
-# wbregs reset 0
-wbregs scope 0x020
-wbregs bounces 0
-wbregs bounces
+################################################################################
+##
+## Filename:	reset.sh
+##
+## Project:	Debouncer project, a learning project to learn the impact
+##		of bouncing on logic within your device.
+##
+## Purpose:	To clear the bounce metrics and scope, preparing them for
+##		another capture / keypress.
+##
+## Creator:	Dan Gisselquist, Ph.D.
+##		Gisselquist Technology, LLC
+##
+################################################################################
+##
+## Copyright (C) 2017, Gisselquist Technology, LLC
+##
+## This program is free software (firmware): you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or (at
+## your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+## FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+## for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
+## target there if the PDF file isn't present.)  If not, see
+## <http://www.gnu.org/licenses/> for a copy.
+##
+## License:	GPL, v3, as defined and found on www.gnu.org,
+##		http://www.gnu.org/licenses/gpl.html
+##
+##
+################################################################################
+##
+##
+wbregs scope 0x020	# Reset the scope
+wbregs bounces 0	# Reset the unbounced code
+wbregs bounces		# Verify that the two counts have returned to zero
 wbregs duration
